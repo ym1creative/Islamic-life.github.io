09c092856df5e11d7a318e91fd338a20ede94e8f
ym1creative <yahya1ninja@gmail.com>
2023-12-29 09:21:28 +0200

    new update

--- launch.js
+++ launch.js
@@ -5,6 +5,7 @@ var imageContainer = document.getElementById("bism");
 var playlistContainer = document.getElementById('sharawi');
 var playlistContainer1 = document.getElementById('playlsit');
 
+
 function changeVideo(videoId) {
     document.getElementById('youtube-player').src = 'https://www.youtube.com/embed/' + videoId;
 }
@@ -45,13 +46,21 @@ if (playlistContainer1.style.display === 'none') {
     document.getElementById('showPlaylistBtn1').innerText = 'Show Playlist';
 }
 }
+function toggleMainDiv() {
+  var mainDiv = document.getElementById("main");
+  if (mainDiv.style.display === "none" || mainDiv.style.display === "") {
+      mainDiv.style.display = "flex";
+  } else {
+      mainDiv.style.display = "none";
+  }
+}
 function changeVideo1() {
 // Get the video element and source element
 var video = document.getElementById("myVideo");
 var source = document.getElementById("videoSource");
 
 // Define the new video source
-var newSource = "pexels-sosa-films-6773297 (1080p).mp4";
+var newSource = "pexels-sosa-films-6773297 (1080p).mp4m";
 
 // Update the source element's src attribute
 source.src = newSource;
@@ -122,3 +131,115 @@ function changePage(delta) {
 
 // Initial display
 showPage(currentPage);
+//getting all elements of html in js
+let song_image = document.getElementById("cover_photo")
+let song_title = document.getElementById("title")
+let song_singer = document.getElementById("singer")
+let song_play = document.getElementById("play")
+let song_prev = document.getElementById("prev")
+let song_next = document.getElementById("next")
+ 
+ 
+ 
+console.log(song_play)
+ 
+ 
+//creating song object list
+let songs_list = [
+    {
+        name : 'سوره الكهف',
+        Image : '',
+        Song : 'a.mp3',
+        Singer : 'المنشاوي'
+    },
+    {
+        name : 'Love Story',
+        Image : 'images/taylor_swift_love_story.jpg',
+        Song : 'music/Taylor-Swift-Love-Story.mp3',
+        Singer : 'Taylor Swift'
+    },
+    {
+        name : 'Red',
+        Image : 'images/Taylor_Swift_Red.png',
+        Song : 'music/Taylor-Swift-Red.mp3',
+        Singer : 'Taylor Swift'
+    },
+    {
+        name : 'Wildest Dreams',
+        Image : 'images/wildest_dreams_taylor.jfif',
+        Song : 'music/Taylor-Swift-Wildest-Dreams.mp3',
+        Singer : 'Taylor Swift'
+    }
+]
+ 
+//keeping track of which song is playing and if song is playing or not
+let i=0;
+let flag=false;
+ 
+ 
+//showing song name and image on screen
+var audio = new Audio(songs_list[i].Song);
+song_image.src = songs_list[i].Image
+song_title.innerHTML = songs_list[i].name
+song_singer.innerHTML = songs_list[i].Singer
+ 
+//function to play/pause song
+song_play.addEventListener("click", function(){
+ 
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+    else{
+        audio.pause()
+        flag=false;
+        console.log(flag)
+    }
+})
+ 
+//function to play next song
+song_next.addEventListener("click", function(){
+   
+    audio.pause()
+    flag = false;
+    i = i+1;
+    if(i>=songs_list.length){
+       
+        i=0;
+    }
+    console.log(i)
+    song_image.src = songs_list[i].Image
+    song_singer.innerHTML = songs_list[i].Singer
+    song_title.innerHTML = songs_list[i].name
+    audio = new Audio(songs_list[i].Song)
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+   
+})
+ 
+//function to play previous song
+song_prev.addEventListener("click", function(){
+   
+    audio.pause()
+    flag = false;
+    i = i-1;
+    if(i<=0){
+        i=songs_list.length-1;
+        console.log(i)
+    }
+    song_image.src = songs_list[i].Image
+    song_singer.innerHTML = songs_list[i].Singer
+    song_title.innerHTML = songs_list[i].name
+    audio = new Audio(songs_list[i].Song)
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+   
+   
+})
